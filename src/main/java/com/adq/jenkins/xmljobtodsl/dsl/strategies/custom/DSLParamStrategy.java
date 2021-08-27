c79dc4a395da49631e5b06d62e59865cd5d04963
Jacek Pintera <jacek.pintera@tomtom.com>
2021-08-27 14:26:28 +0200

    ccc

--- src/main/java/com/adq/jenkins/xmljobtodsl/dsl/strategies/custom/DSLParamStrategy.java
+++ src/main/java/com/adq/jenkins/xmljobtodsl/dsl/strategies/custom/DSLParamStrategy.java
@@ -6,32 +6,34 @@ import com.adq.jenkins.xmljobtodsl.parsers.PropertyDescriptor;
 
 public class DSLParamStrategy extends DSLMethodStrategy {
 
-	private final String methodName;
+    private final String methodName;
 
-	public DSLParamStrategy(int tabs, PropertyDescriptor propertyDescriptor, String methodName) {
-		super(tabs, propertyDescriptor, methodName);
-		this.methodName = methodName;
-	}
+    public DSLParamStrategy(int tabs, PropertyDescriptor propertyDescriptor, String methodName) {
+        super(tabs, propertyDescriptor, methodName);
+        this.methodName = methodName;
+    }
 
-	@Override
-	public String toDSL() {
-		return replaceTabs(String.format(getSyntax("syntax.method_call"),
-				methodName, getOrderedChildrenDSL()), getTabs());
-	}
+    @Override
+    public String toDSL() {
+        return replaceTabs(String.format(getSyntax("syntax.method_call"),
+                                         methodName, getOrderedChildrenDSL()), getTabs());
+    }
 
-	public String getOrderedChildrenDSL() {
-		String defaultValue = getChildrenByName("defaultValue").toDSL();
-		String name = getChildrenByName("name").toDSL();
-		String description = getChildrenByName("description").toDSL();
-		return name + ", " + defaultValue + ", " + description;
-	}
+    public String getOrderedChildrenDSL() {
+        final DSLStrategy defaultValue1 = getChildrenByName("defaultValue");
+        String defaultValue = defaultValue1 == null ? "\"\"" : defaultValue1.toDSL();
+        String name = getChildrenByName("name").toDSL();
+        final DSLStrategy description1 = getChildrenByName("description");
+        String description = description1 == null ? "\"\"" : description1.toDSL();
+        return name + ", " + defaultValue + ", " + description;
+    }
 
-	private DSLStrategy getChildrenByName(String name) {
-		for (DSLStrategy strategy : getChildren()) {
-			if (strategy.getDescriptor().getName().equals(name)) {
-				return strategy;
-			}
-		}
-		throw new RuntimeException(String.format("Child with name: %s not found", name));
-	}
+    private DSLStrategy getChildrenByName(String name) {
+        for (DSLStrategy strategy : getChildren()) {
+            if (strategy.getDescriptor().getName().equals(name)) {
+                return strategy;
+            }
+        }
+        return null;
+    }
 }
